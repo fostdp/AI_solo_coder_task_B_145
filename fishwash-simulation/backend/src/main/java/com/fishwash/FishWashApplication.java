23d3b28e29aad09eac907bec63d098f60dced906
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-18 07:37:55 +0800

    Update project

---
+++ fishwash-simulation/backend/src/main/java/com/fishwash/FishWashApplication.java
@@ -0,0 +1,12 @@
+package com.fishwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishWashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishWashApplication.class, args);
+    }
+}
